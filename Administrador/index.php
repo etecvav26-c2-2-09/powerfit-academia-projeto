8f602091fa655f28dcd42e4eec029759012ec9a3
Gabriel Oliveira <gabrielbubu48@gmail.com>
2026-09-24 11:16:50 -0300

    Delete Administrador/index.php

--- Administrador/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Administrador/index.php - Ponto de entrada do painel administrativo.
- * Autor: Gabriel Oliveira
- * Envia para a listagem de Planos (troque o destino se preferir outra tela inicial).
- */
-header('Location: planos/index.php');
-exit;
