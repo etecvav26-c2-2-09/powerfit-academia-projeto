0d9f606a32460c809e26b1cc4110d3ea5e95572f
Gabriel Oliveira <gabrielbubu48@gmail.com>
2026-09-24 11:12:59 -0300

    Add files via upload

---
+++ Administrador/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Administrador/index.php - Ponto de entrada do painel administrativo.
+ * Autor: Gabriel Oliveira
+ * Envia para a listagem de Planos (troque o destino se preferir outra tela inicial).
+ */
+header('Location: planos/index.php');
+exit;
